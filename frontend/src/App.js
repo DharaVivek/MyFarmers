33c14e3637918bb481dbc0f9a266bae303e5b76d
DharaVivek <dharavivek.s@gmail.com>
2023-01-25 10:52:08 +0530

    step2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
+import data from './data';
 function App() {
   return (
     <div>
       <header>
         <a href="/">MyFarmer</a>
       </header>
-      <main>All Crops</main>
+      <main>
+        <h1>Crops</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
