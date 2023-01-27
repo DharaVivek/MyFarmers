2e03891518d6d5b3377ab29426f7adda3ee5d7f7
DharaVivek <dharavivek.s@gmail.com>
2023-01-27 14:32:42 +0530

    step17

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -57,7 +57,7 @@ function ProductScreen() {
     const quantity = existItem ? existItem.quantity + 1 : 1;
     const { data } = await axios.get(`/api/products/${product._id}`);
     if (data.countInStock < quantity) {
-      window.alert('Sorry. Product is out of stock');
+      window.alert('Product is out of stock');
       return;
     }
     ctxDispatch({
