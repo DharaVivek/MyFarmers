69e9e89060a9a7bc2ae3e0ee6fc47ea10f67b3a3
DharaVivek <dharavivek.s@gmail.com>
2023-01-28 10:40:40 +0530

    123

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, unique: true },
+    name: { type: String, required: true},
     slug: { type: String, required: true, unique: true },
     image: { type: String, required: true },
     brand: { type: String, required: true },
