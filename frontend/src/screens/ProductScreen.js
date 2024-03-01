74cc0ccfa6abeb1a837c2ce7243819fd9bc6734a
DharaVivek <95916493+DharaVivek@users.noreply.github.com>
2024-03-02 05:05:50 +0530

    Update ProductScreen.js

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -108,7 +108,7 @@ function ProductScreen() {
               <ListGroup variant="flush">
                 <ListGroup.Item>
                   <Row>
-                    <Col>Price per ton:</Col>
+                    <Col>Price :</Col>
                     <Col>₹{product.price}</Col>
                   </Row>
                 </ListGroup.Item>
@@ -117,7 +117,7 @@ function ProductScreen() {
                     <Col>Status:</Col>
                     <Col>
                       {product.countInStock > 0 ? (
-                        <Badge bg="success">In Stock</Badge>
+                        <Badge bg="success">Available</Badge>
                       ) : (
                         <Badge bg="danger">Unavailable</Badge>
                       )}
