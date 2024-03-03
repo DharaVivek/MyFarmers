208b91935122d34d9f32bce9c4e0ff4679d894b7
DharaVivek <dharavivek.s@gmail.com>
2024-03-03 20:30:35 +0530

    Revert "Update CheckoutSteps.js"
    This reverts commit 41829960ec667c3dbc9c2b9bdc75df580120bdad.

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -6,7 +6,7 @@ export default function CheckoutSteps(props) {
   return (
     <Row className="checkout-steps">
       <Col className={props.step1 ? 'active' : ''}>Sign-In</Col>
-      <Col className={props.step2 ? 'active' : ''}>Details</Col>
+      <Col className={props.step2 ? 'active' : ''}>Shipping</Col>
       <Col className={props.step3 ? 'active' : ''}>Payment</Col>
       <Col className={props.step4 ? 'active' : ''}>Place Order</Col>
     </Row>
