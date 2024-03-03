c512432c13e9ecb50899f5524528e9112fce99c1
DharaVivek <dharavivek.s@gmail.com>
2024-03-03 20:31:35 +0530

    Revert "Update CartScreen.js"
    This reverts commit 814c9877de4625b717383a935fa56666718da547.

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -46,7 +46,7 @@ export default function CartScreen() {
         <Col md={8}>
           {cartItems.length === 0 ? (
             <MessageBox>
-              Cart is empty. <Link to="/">Go pick some packages</Link>
+              Cart is empty. <Link to="/">Go Shopping</Link>
             </MessageBox>
           ) : (
             <ListGroup>
