a9cd7d578a7196877bad85f478337f7c4f123085
DharaVivek <dharavivek.s@gmail.com>
2024-03-03 20:30:56 +0530

    Revert "Update ShippingAddressScreen.js"
    This reverts commit 56ac6c3ebe8952af305243dffa28d7748e7836e8.

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -52,15 +52,15 @@ export default function ShippingAddressScreen() {
   return (
     <div>
       <Helmet>
-        <title>Details</title>
+        <title>Shipping Address</title>
       </Helmet>
 
       <CheckoutSteps step1 step2></CheckoutSteps>
       <div className="container small-container">
-        <h1 className="my-3">Details</h1>
+        <h1 className="my-3">Shipping Address</h1>
         <Form onSubmit={submitHandler}>
           <Form.Group className="mb-3" controlId="fullName">
-            <Form.Label>Company Name</Form.Label>
+            <Form.Label>Full Name</Form.Label>
             <Form.Control
               value={fullName}
               onChange={(e) => setFullName(e.target.value)}
@@ -68,7 +68,7 @@ export default function ShippingAddressScreen() {
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="address">
-            <Form.Label>Description ans goals</Form.Label>
+            <Form.Label>Address</Form.Label>
             <Form.Control
               value={address}
               onChange={(e) => setAddress(e.target.value)}
@@ -76,7 +76,7 @@ export default function ShippingAddressScreen() {
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="city">
-            <Form.Label>Location</Form.Label>
+            <Form.Label>City</Form.Label>
             <Form.Control
               value={city}
               onChange={(e) => setCity(e.target.value)}
@@ -84,7 +84,7 @@ export default function ShippingAddressScreen() {
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="postalCode">
-            <Form.Label>Phone number</Form.Label>
+            <Form.Label>Postal Code</Form.Label>
             <Form.Control
               value={postalCode}
               onChange={(e) => setPostalCode(e.target.value)}
@@ -92,7 +92,7 @@ export default function ShippingAddressScreen() {
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="country">
-            <Form.Label>Tech stack</Form.Label>
+            <Form.Label>Country</Form.Label>
             <Form.Control
               value={country}
               onChange={(e) => setCountry(e.target.value)}
